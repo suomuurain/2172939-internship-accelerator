d1087160fb70000b011804028283bdae11ca5bdc
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-01-13 16:30:59 +0300

    подключает аккордеоны

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,28 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
+import {iosVhFix} from './utils/ios-vh-fix';
+import {Form} from './vendor/form-validate/form';
+import {initAccordions} from './vendor/accordions/init-accordions';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    const form = new Form();
+    window.form = form;
+    form.init();
+    initAccordions();
+  });
+});
