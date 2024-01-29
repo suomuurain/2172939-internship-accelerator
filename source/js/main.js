4158efa2dc26d82415d6ba2570fbcca2fbfc9348
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-01-29 23:15:23 +0300

    добавляет модалку

--- source/js/main.js
+++ source/js/main.js
@@ -12,6 +12,7 @@ import {initProgramsSwiper} from './modules/programs-swiper';
 import {initNewsSwiper} from './modules/news-swiper';
 import {initAccordions} from './vendor/accordions/init-accordions';
 import {CustomSelect} from './vendor/custom-select/custom-select';
+import {initModals} from "./vendor/modals/init-modals";
 
 // ---------------------------------
 
@@ -33,6 +34,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
 
+    initModals();
     initAccordions();
     initProgramsSwiper();
     initNewsSwiper();
