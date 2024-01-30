211a3bcc4b3e6bffbfb2708d5b9eceaff853516a
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-01-30 20:49:02 +0300

    добавляет бургерное меню и стики хедер

--- source/js/main.js
+++ source/js/main.js
@@ -6,6 +6,9 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './vendor/form-validate/form';
 
+import {Burger} from './vendor/header/burger';
+import {StickyHeader} from './vendor/header/sticky-header';
+
 import {initHeroSwiper} from './modules/hero-swiper';
 import {initReviewsSwiper} from './modules/reviews-swiper';
 import {initProgramsSwiper} from './modules/programs-swiper';
@@ -30,6 +33,11 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const burger = new Burger();
+    burger.init();
+    const stickyHeader = new StickyHeader();
+    stickyHeader.init();
+
     const form = new Form();
     window.form = form;
     form.init();
