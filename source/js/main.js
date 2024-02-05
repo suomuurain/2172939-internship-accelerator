3f2f6e70126bf70ec34d8735ce328fbc958c54a4
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-02-06 00:03:30 +0300

    удаляет закомментированный код

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './vendor/form-validate/form';
 
