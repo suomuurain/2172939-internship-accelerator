ee016d14de14c275d3261e28f5a4edb5ef6e07c7
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-02-06 12:16:04 +0300

    добавляет скрипт

---
+++ source/js/modules/menuToggel.js
@@ -0,0 +1,29 @@
+// import {ScrollLock} from '../../utils/scroll-lock';
+
+// const initMenuToggel = () => {
+//   const header = document.querySelector('.header');
+//   const burgerButton = header.querySelector('.header__burger');
+//   const navigation = header.querySelector('.header__navigation');
+//   const link = header.querySelector('[data-link]');
+//   const isOpened = 'is-open';
+
+//   const onLinkClick = (evt) => {
+//     return evt.target.matches(link) ? closeMenu() : null;
+//   };
+
+//   const openMenu = () => {
+//     header.classList.add(isOpened);
+//     window.scrollLock.disableScrolling();
+//   }
+
+//   const closeMenu = () => {
+//     header.classList.remove(isOpened);
+//     window.scrollLock.enableScrolling();
+//   }
+
+//   navigation.addEventListener("click", () => {
+
+//   });
+// }
+
+// export {initMenuToggel};
